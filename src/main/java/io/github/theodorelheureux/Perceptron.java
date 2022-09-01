f9db945a153bd4e7fcd3b771af5e0590600e0316
Théodore L’Heureux <theodorelheureux@gmail.com>
2022-09-01 15:38:17 +0000

    Create project and Perceptron class

---
+++ src/main/java/io/github/theodorelheureux/Perceptron.java
@@ -0,0 +1,19 @@
+package main.java.io.github.theodorelheureux;
+
+import java.util.Random;
+
+public class Perceptron {
+    
+    Random r;
+
+    public Perceptron(float eta, int nIter, long randomSeed) {
+
+        r = new Random(randomSeed);
+
+    }
+
+    public void fit() {
+        
+    }
+
+}
